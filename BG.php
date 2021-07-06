47ea48c8a1bdcee446ade9bb410df69a41fe00dd
Moha-01 <haji.m@web.de>
2021-07-06 17:35:53 +0200

    More ideas!!!

---
+++ BG.php
@@ -0,0 +1,15 @@
+<?php
+
+include "./html/head.html";
+include "./html/background.html";
+include "./html/header.html";
+
+//hier die Seite angeben!
+echo "<style>aside{visibility: hidden;}</style>";
+include "./html/aside.html";
+
+
+include "./html/footer.html";
+include "./html/foot.html";
+
+ ?>
