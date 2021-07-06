2ef35278feb47546ba8108b57745551ec2b92dfc
DESKTOP-78NCDQS\Lukas <luk.hautzinger.20@lehre.mosbach.dhbw.de>
2021-07-06 20:52:39 +0200

    colors page added

---
+++ colors.php
@@ -0,0 +1,14 @@
+<?php
+
+include "./html/head.html";
+include "./html/background.html";
+include "./html/header.html";
+
+//hier die Seite angeben!
+include "./html/colors.html";
+
+
+include "./html/footer.html";
+include "./html/foot.html";
+
+ ?>
